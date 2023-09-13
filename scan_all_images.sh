12e8311bb81410e65e8a70aa72f15d79d468e3ab
Faye <faye@kosli.com>
2023-09-13 16:53:29 +0200

    More minor fixes

--- scan_all_images.sh
+++ scan_all_images.sh
@@ -7,23 +7,21 @@ export KOSLI_ORG=cyber-dojo
 
 kosli_fetch_snapshot()
 {
-    snapshot=$(mktemp /tmp/snapshot.json)
-    kosli get snapshot aws-prod -o json > $snapshot
+    kosli get snapshot aws-prod -o json > snapshot.json
 
-    flows=($(jq -r '.[].flow' $snapshot))
-    gits=($(jq -r '.[].git_commit' $snapshot))
-    fingerprints=($(jq -r '.[].fingerprint' $snapshot))
-    artifacts=($(jq -r '.[].artifact' $snapshot))
+    flows=($(jq -r '.[].flow' snapshot.json))
+    gits=($(jq -r '.[].git_commit' snapshot.json))
+    fingerprints=($(jq -r '.[].fingerprint' snapshot.json))
+    artifacts=($(jq -r '.[].artifact' snapshot.json))
 
-    rm "$snapshot"
+    rm "snapshot.json"
 }
 
 kosli_get_build()
 {
-    tempfile=$(mktemp /tmp/build.json)
-    kosli get artifact "$FLOW@$FINGERPRINT" -o json > $tempfile
-    build=($(jq -r '.build_url' $tempfile))
-    rm "$tempfile"
+    kosli get artifact "$FLOW@$FINGERPRINT" -o json > build.json
+    build=($(jq -r '.build_url' build.json))
+    rm "build.json"
     echo $build
 }
 
@@ -58,7 +56,7 @@ for i in ${!flows[@]}; do
     BUILD=$(kosli_get_build)
 
     #Only run for the creator service right now
-    if [ $FLOW != "snyk_scans" ]; then
+    if [ $FLOW == "creator" ]; then
 
         run_snyk_scan
         send_to_kosli
