400cc33446c95153c9e6f55b93a8cd637233f95d
zhelezovartem <Zhelezovartem@gmail.com>
2023-10-12 14:43:45 +0600

    set correct image name

--- snyk_scan_live_artifacts_and_report_any_new_vulnerabilities_to_kosli.sh
+++ snyk_scan_live_artifacts_and_report_any_new_vulnerabilities_to_kosli.sh
@@ -57,7 +57,7 @@ run_snyk_scan()
 {
     local -r snyk_output_json_filename="${1}"
     # Use fingerprint in image name for absolute certainty of image's identity.
-    local -r image_name="cyberdojo/${FLOW}@sha256:${FINGERPRINT}"
+    local -r image_name="${IMAGE_NAME}@sha256:${FINGERPRINT}"
     local -r snyk_policy_filename=.snyk
 
     # All cyber-dojo microservice repos hold a .snyk policy file.
