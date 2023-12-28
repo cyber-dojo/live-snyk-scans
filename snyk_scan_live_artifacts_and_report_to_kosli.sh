5efb450a4fbae825bea60eac987be22389590b94
JonJagger <jon@kosli.com>
2023-12-28 14:26:10 +0000

    Use artifact-name and fingerprint as reported in snapshot

--- snyk_scan_live_artifacts_and_report_to_kosli.sh
+++ snyk_scan_live_artifacts_and_report_to_kosli.sh
@@ -26,9 +26,9 @@ snyk_scan_live_artifacts_and_report_any_new_vulnerabilities_to_kosli()
         if [ "${annotation_type}" != "exited" ]; then
           flow=$(jq -r ".artifacts[$i].flow_name" ${snapshot_json_filename})
           git_commit=$(jq -r ".artifacts[$i].git_commit" ${snapshot_json_filename})
-          fingerprint=$(jq -r ".artifacts[$i].fingerprint" ${snapshot_json_filename})
           artifact_name=$(jq -r ".artifacts[$i].name" ${snapshot_json_filename})
-          report_snyk_vulnerabilities_to_kosli "${flow}" "${git_commit}" "${fingerprint}" "${artifact_name}"
+          fingerprint=$(jq -r ".artifacts[$i].fingerprint" ${snapshot_json_filename})
+          report_snyk_vulnerabilities_to_kosli "${flow}" "${git_commit}" "${artifact_name}" "${fingerprint}"
        fi
     done
 }
@@ -37,25 +37,24 @@ report_snyk_vulnerabilities_to_kosli()
 {
     local -r flow="${1}"        # eg differ
     local -r git_commit="${2}"  # eg 44e6c271b46a56acd07f3b426c6cbca393442bb4
-    local -r fingerprint="${3}" # eg c6cd1a5b122d88aaeb41c1fdd015ad88c2bea95ae85f63eb5544fb707254847e
-    local -r image_name="${4}"  # eg 274425519734.dkr.ecr.eu-central-1.amazonaws.com/differ:44e6c27
-
-    local -r snyk_output_json_filename=snyk.json
-    local -r snyk_policy_filename=.snyk
+    local -r image_name="${3}"  # eg 274425519734.dkr.ecr.eu-central-1.amazonaws.com/differ:44e6c27
+    local -r fingerprint="${4}" # eg c6cd1a5b122d88aaeb41c1fdd015ad88c2bea95ae85f63eb5544fb707254847e
 
     if [ "${flow}" == "" ]; then
       echo "Artifact ${image_name} in Environment ${KOSLI_ENVIRONMENT} has no provenance in ${KOSLI_HOST}"
       return
     fi
 
+    local -r snyk_output_json_filename=snyk.json
+    local -r snyk_policy_filename=.snyk
+
     # All cyber-dojo microservice repos hold a .snyk policy file.
     # This is an empty file when no vulnerabilities are turned-off.
     # Ensure we get the .snyk file for the given artifact's git commit.
     curl "https://raw.githubusercontent.com/cyber-dojo/${flow}/${git_commit}/.snyk"  > "${snyk_policy_filename}"
 
-    # Use fingerprint in image name for absolute certainty of image's identity.
     set +e
-    snyk container test "${image_name}@sha256:${fingerprint}" \
+    snyk container test "${image_name}" \
         --json-file-output="${snyk_output_json_filename}" \
         --severity-threshold=medium \
         --policy-path="${snyk_policy_filename}"
@@ -65,17 +64,15 @@ report_snyk_vulnerabilities_to_kosli()
       --description="Scan of deployed Artifacts running in their Environment" \
       --template=artifact,snyk-scan
 
-    docker pull "${image_name}"
-
     kosli report artifact "${image_name}" \
-        --artifact-type=docker
+      --fingerprint="${fingerprint}"
 
-    kosli report evidence artifact snyk \
+    kosli report evidence artifact snyk "${image_name}" \
       --fingerprint="${fingerprint}" \
       --name=snyk-scan \
       --scan-results="${snyk_output_json_filename}"
 
-    kosli expect deployment \
+    kosli expect deployment "${image_name}" \
       --fingerprint="${fingerprint}" \
       --description="Deployed in ${KOSLI_ENVIRONMENT}" \
       --environment="${KOSLI_ENVIRONMENT}"
