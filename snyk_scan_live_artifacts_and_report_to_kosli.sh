ec1ff5e3b2ce175fd10f67971de603f5da3a135e
JonJagger <jon@kosli.com>
2024-01-11 08:23:03 +0000

    CI: report to each Flow as well as to a dedicated flow for one service only

--- snyk_scan_live_artifacts_and_report_to_kosli.sh
+++ snyk_scan_live_artifacts_and_report_to_kosli.sh
@@ -15,7 +15,7 @@ snyk_scan_live_artifacts_and_report_any_new_vulnerabilities_to_kosli()
 {
     local -r snapshot_json_filename=snapshot.json
 
-    # Use Kosli CLI to get info on what artifacts are currently running in production
+    # Use Kosli CLI to get info on what artifacts are currently running in the given environment
     # (docs/snapshot.json contains an example json file)
     kosli get snapshot "${KOSLI_ENVIRONMENT}" --output=json > "${snapshot_json_filename}"
     # Process info, one artifact at a time
@@ -25,10 +25,14 @@ snyk_scan_live_artifacts_and_report_any_new_vulnerabilities_to_kosli()
         annotation_type=$(jq -r ".artifacts[$i].annotation.type" ${snapshot_json_filename})
         if [ "${annotation_type}" != "exited" ]; then
           flow=$(jq -r ".artifacts[$i].flow_name" ${snapshot_json_filename})
-          git_commit=$(jq -r ".artifacts[$i].git_commit" ${snapshot_json_filename})
           artifact_name=$(jq -r ".artifacts[$i].name" ${snapshot_json_filename})
-          fingerprint=$(jq -r ".artifacts[$i].fingerprint" ${snapshot_json_filename})
-          report_snyk_vulnerabilities_to_kosli "${flow}" "${git_commit}" "${artifact_name}" "${fingerprint}"
+          if [ "${flow}" == "" ]; then
+            echo "Artifact ${artifact_name} in Environment ${KOSLI_ENVIRONMENT} has no provenance in ${KOSLI_HOST}"
+          else
+            git_commit=$(jq -r ".artifacts[$i].git_commit" ${snapshot_json_filename})
+            fingerprint=$(jq -r ".artifacts[$i].fingerprint" ${snapshot_json_filename})
+            report_snyk_vulnerabilities_to_kosli "${flow}" "${git_commit}" "${artifact_name}" "${fingerprint}"
+          fi
        fi
     done
 }
@@ -36,18 +40,43 @@ snyk_scan_live_artifacts_and_report_any_new_vulnerabilities_to_kosli()
 report_snyk_vulnerabilities_to_kosli()
 {
     local -r flow="${1}"          # eg differ
-    local -r git_commit="${2}"    # eg 44e6c271b46a56acd07f3b426c6cbca393442bb4
-    local -r artifact_name="${3}" # eg 274425519734.dkr.ecr.eu-central-1.amazonaws.com/differ:44e6c27
-    local -r fingerprint="${4}"   # eg c6cd1a5b122d88aaeb41c1fdd015ad88c2bea95ae85f63eb5544fb707254847e
+    local -r git_commit="${1}"    # eg 44e6c271b46a56acd07f3b426c6cbca393442bb4
+    local -r artifact_name="${2}" # eg 274425519734.dkr.ecr.eu-central-1.amazonaws.com/differ:44e6c27
+    local -r fingerprint="${3}"   # eg c6cd1a5b122d88aaeb41c1fdd015ad88c2bea95ae85f63eb5544fb707254847e
 
-    if [ "${flow}" == "" ]; then
-      echo "Artifact ${artifact_name} in Environment ${KOSLI_ENVIRONMENT} has no provenance in ${KOSLI_HOST}"
-      return
+    if [ "${flow}" == "languages-start-points" ]; then
+      # For one micro-service only, experiment with reporting to dedicated flow
+      report_snyk_vulnerabilities_to_kosli_in_dedicated_flow "${flow}" "${git_commit}" "${artifact_name}" "${fingerprint}"
     fi
 
-    if [ "${flow}" != "languages-start-points" ]; then
-      return  # Do only one service till all working
-    fi
+    local -r snyk_output_json_filename=snyk.json
+    local -r snyk_policy_filename=.snyk
+
+    # All cyber-dojo microservice repos hold a .snyk policy file.
+    # This is an empty file when no vulnerabilities are turned-off.
+    # Ensure we get the .snyk file for the given artifact's git commit.
+    curl "https://raw.githubusercontent.com/cyber-dojo/${flow}/${git_commit}/.snyk"  > "${snyk_policy_filename}"
+
+    set +e
+    snyk container test "${artifact_name}@sha256:${fingerprint}" \
+        --json-file-output="${snyk_output_json_filename}" \
+        --severity-threshold=medium \
+        --policy-path="${snyk_policy_filename}"
+    set -e
+
+    kosli report evidence artifact snyk \
+      --fingerprint="${fingerprint}" \
+      --flow="${flow}" \
+      --name=snyk-scan \
+      --scan-results="${snyk_output_json_filename}"
+}
+
+report_snyk_vulnerabilities_to_kosli_in_dedicated_flow()
+{
+    local -r flow="${1}"          # eg differ
+    local -r git_commit="${1}"    # eg 44e6c271b46a56acd07f3b426c6cbca393442bb4
+    local -r artifact_name="${2}" # eg 274425519734.dkr.ecr.eu-central-1.amazonaws.com/differ:44e6c27
+    local -r fingerprint="${3}"   # eg c6cd1a5b122d88aaeb41c1fdd015ad88c2bea95ae85f63eb5544fb707254847e
 
     local -r snyk_output_json_filename=snyk.json
     local -r snyk_policy_filename=.snyk
@@ -81,6 +110,7 @@ report_snyk_vulnerabilities_to_kosli()
       --description="Deployed in ${KOSLI_ENVIRONMENT}" \
       --environment="${KOSLI_ENVIRONMENT}"
 }
+
 
 exit_non_zero_unless_installed kosli snyk jq
 snyk_scan_live_artifacts_and_report_any_new_vulnerabilities_to_kosli
