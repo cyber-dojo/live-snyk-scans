337ca7d5a3a29dd6ab4857657aa36578a64661f3
JonJagger <jon@kosli.com>
2026-03-13 18:59:48 +0000

    Run workflow with TEST flow

---
+++ bin/filter_sarif.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sarif_filename = sys.argv[1]
+
+    # Extract ids and severities of each vulnerability in sarif file
+    with open(sarif_filename) as sarif_file:
+        sarif_data = json.load(sarif_file)
+
+    vulns = []
+    for run in sarif_data['runs']:
+        for rule in run['tool']['driver']['rules']:
+            id = rule['id']
+            url = f"https://security.snyk.io/vuln/{id}"
+            short_text = rule['shortDescription']['text']
+            #cvssv3_base_score = rule['properties']['cvssv3_baseScore'] # eg 6.8 can be None
+            #security_severity = rule['properties']['security-severity'] # eg 6.8 can be None
+            severity = short_text.split(' ')[0].lower()  # eg "medium"
+            assert severity in ["critical", "high", "medium", "low"]
+
+            vulns.append({
+                'id': id,
+                'severity': severity,
+                'url': url
+            })
+
+    print(json.dumps(vulns, default=str))
+
+
+#   Severity. CVSS v3 Rating
+#   ------------------------
+#   Critical. 9.0 - 10.0
+#   High	  7.0 -  8.9
+#   Medium	  4.0 -  6.9
+#   Low	      0.1 -  3.9
